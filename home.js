37a38a5059cd66f2d12c210653c672d9a802a506
Mark Dohle (MIT) <104167303+markdohle@users.noreply.github.com>
2022-10-05 16:15:22 -0400

    Delete home.js

--- home.js
+++
@@ -1,9 +0,0 @@
-function Home() {
-    const ctx = React.useContext(UserContext);
-    return(
-        <div>
-            <h3>Home</h3>
-            {JSON.stringify(ctx.users)}
-        </div>
-    );
-};
